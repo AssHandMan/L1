37b06349646c834c150b848f2b03efd0b327e6bf
AssHandMan <85223913+AssHandMan@users.noreply.github.com>
2025-05-08 03:16:28 +0300

    Lab9-10

---
+++ Lab10/Lab10/Lab10.cpp
@@ -0,0 +1,201 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <string>
+#include <windows.h>
+
+enum ConsoleColor
+{
+	Black = 0,
+	Blue = 1,
+	Green = 2,
+	Red = 4,
+	Brown = 6,
+	White = 15,
+	Gray = 8
+};
+
+void SetColor(ConsoleColor text, ConsoleColor background)
+{
+	HANDLE hConsoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsoleHandle, (WORD)((background << 4) | text));
+}
+
+void setCursorPosition(int x, int y)
+{
+	static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	std::cout.flush();
+	COORD coord = { (SHORT)x, (SHORT)y };
+	SetConsoleCursorPosition(hOut, coord);
+}
+using namespace std;
+int CountOfCoins = 0;
+int posX = 2;
+int posY = 3;
+int lustPosX = 0;
+int lustPosY = 0;
+
+int q[48] = { 1, 1, 1, 1, 1, -1, 1, -1, 1, -1, -1, 1, -1, 1, 1 , 1 , 1, 1, 1, 1, 1, -1, 1, 1, -1, -1, 1, -1, 1, -1, -1, -1, 1, -1, 1, -1, -1, 1, 1, 1, -1, -1, 1, 1, -1, -1, -1, 1 };
+
+int Map[30][50]{
+	3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 991, 99, 993, 3,
+	3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, -1, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3,
+	3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 4, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+	3, 1, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 0, -1, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 4, 0, 4, 0, 0, 0, 4, 4, 4, 4, 4, 3,
+	3, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 10, 0, 4, 4, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 4, 0, 0, 0, 4, 0, 0, 0, 4, 3,
+	3, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 4, 0, 0, 4, 0, 0, 4, 0, 0, 3,
+	3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 4, 4, 0, 0, 0, 0, 9, 0, 0, 0, 4, 4, 0, 0, 3, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 4, 0, 3,
+	3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 4, 4, 0, 0, 13, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3,
+	3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 3, 0, 4, 0, 4, 0, 0, 4, 0, 4, 0, 4, 4, 4, 0, 3,
+	3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 11, 3,
+	3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 3, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+	3, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, -1, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 4, 0, 4, 0, 0, 4, 0, 0, 4, 0, 0, 4, 3,
+	3, 0, 0, 0, 0, 17, 0, 0, 0, 0, 0, 0, 0, 4, 4, 0, 0, 26, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+	3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 23, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 3,
+	3, 0, 0, 0, 0, 0, 0, 0, 18, 0, 0, 0, 0, 0, 4, 4, 0, 0, 21, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 3,
+	3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 19, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 22, 0, 0, 4, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 24, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+	3, 0, 0, 0, 20, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+	3, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+	3, 15, 0, 0, 3, 3, 3, 0, 0, 0, 29, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 0, 37, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 45, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+	3, 0, 3, 3, 3, 0, 28, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 0, 0, 0, 35, 0, 0, 3, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+	3, 0, 0, 0, 3, 0, 0, 27, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 34, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 46, 0, 0, 0, 3,
+	3, 0, 0, 0, 3, -1, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+	3, 0, 0, 0, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 33, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 47, 0, 0, 0, 0, 3,
+	3, 4, 0, 0, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 39, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+	3, 0, 0, 0, 0, 0, 32, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 44, 0, 0, 0, 0, 0, 0, 0, 3,
+	3, 0, 0, 0, 0, 0, 0, 0, 0, 31, 0, 0, 0, 4, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 43, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+	3, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 42, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+	3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 30, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+	3, 26, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, -1, 3,
+	3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3
+};
+void Render()
+{
+	system("cls");
+	for (int y = 0; y != 30; y++)
+	{
+		for (int x = 0; x != 50; x++)
+		{
+			if (Map[y][x] == -1) { SetColor(Blue, Blue); cout << " 0"; }
+			else if (Map[y][x] == 1) { SetColor(White, White); cout << " @"; }
+			else if (Map[y][x] == 3) { SetColor(Gray, Gray); cout << " ="; }
+			else if (Map[y][x] == 4) { SetColor(Red, Black); cout << " ,"; }
+			else if (Map[y][x] == 991) { SetColor(Blue, White); cout << " " + to_string(CountOfCoins); }
+			else if (Map[y][x] == 99) { SetColor(Blue, White); cout << " /"; }
+			else if (Map[y][x] == 993) { SetColor(Blue, White); cout << " 5"; }
+			else if (Map[y][x] == 0) { SetColor(Green, Black); cout << " ."; }
+			else { SetColor(Red, Red); cout << " @"; }
+		}
+		cout << endl;
+	}
+}
+
+void Move(int x = 0, int y = 0)
+{
+	lustPosX = posX;
+	lustPosY = posY;
+	posX += x;
+	posY += y;
+}
+
+void Death()
+{
+	setCursorPosition(lustPosX, lustPosY);
+	SetColor(Green, Black); cout << " .";
+	posX = lustPosX = 2; posY = lustPosY = 3;
+	setCursorPosition(lustPosX, lustPosY);
+	SetColor(Green, Black); cout << " .";
+	setCursorPosition(posX, posY);
+	SetColor(White, White); cout << " @";
+	CountOfCoins = 0;
+	setCursorPosition(46 * 2, 0);
+	SetColor(Blue, White); cout << " " + to_string(CountOfCoins);
+	Map[1][35] = -1;
+	Map[3][21] = -1;
+	Map[11][15] = -1;
+	Map[21][5] = -1;
+	Map[28][48] = -1;
+	for (int y = 0; y != 30; y++) {
+		for (int x = 0; x != 50; x++) {
+			if (Map[y][x] == -1) {
+				setCursorPosition(x * 2, y);
+				SetColor(Blue, Blue); cout << " 0";
+			}
+		}
+	}
+}
+
+void Rendering()
+{
+	if (Map[posY][posX / 2] == -1)
+	{
+		CountOfCoins++;
+		setCursorPosition(46 * 2, 0);
+		SetColor(Blue, White); cout << " " + to_string(CountOfCoins);
+		Map[posY][posX / 2] = 0;
+	}
+	if (Map[posY][posX / 2] == 3)
+	{
+		posX = lustPosX;
+		posY = lustPosY;
+	}
+	if (Map[posY][posX / 2] == 0)
+	{
+		setCursorPosition(lustPosX, lustPosY);
+		SetColor(Green, Black); cout << " .";
+		setCursorPosition(posX, posY);
+		SetColor(White, White); cout << " @";
+	}
+	if (Map[posY][posX / 2] == 4) { Death(); }
+
+	int i = 47;
+	while (i != 4)
+	{
+		for (int y = 0; y < 30; y++)
+		{
+			for (int x = 0; x < 50; x++)
+			{
+				if (Map[y][x] == i)
+				{
+					if (x == 49 || x == 0 || Map[y][x + q[i]] == 3 || Map[y][x + q[i]] == 4) { q[i] *= -1; }
+					Map[y][x] = 0;
+					setCursorPosition(x * 2, y);
+					SetColor(Green, Black); cout << " .";
+					Map[y][x + q[i]] = i;
+					setCursorPosition((x + q[i]) * 2, y);
+					SetColor(Red, Red); cout << " @";
+					break;
+				}
+			}
+			if (Map[posY][posX / 2] == i) { Death(); }
+		}
+		i--;
+	}
+	setCursorPosition(100, 30);
+}
+
+int main() {
+
+	Render();
+	int move;
+	while (true)
+	{
+		move = _getch();
+		if (move == 27) { exit(EXIT_SUCCESS); }
+		if (move == 100 && posX < 98) { Move(2, 0); }
+		else if (move == 97 && posX > 1) { Move(-2, 0); }
+		else if (move == 115 && posY < 29) { Move(0, 1); }
+		else if (move == 119 && posY > 0) { Move(0, -1); }
+		Rendering();
+		if (CountOfCoins == 5) { break; }
+
+	}
+	SetColor(White, Black);
+	setCursorPosition(0, 0);
+	system("cls");
+	std::cout << "\n\n";
+	std::cout << "  C O N G R A T U L A T I O N  \n";
+	std::cout << "  C O N G R A T U L A T I O N  \n";
+	std::cout << "  C O N G R A T U L A T I O N  \n";
+	std::cout << "\n\n";
+	SetColor(Black, Black);
+}
---
+++ Lab9/Lab9/Lab9.cpp
@@ -0,0 +1,324 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <memory>
+#include <algorithm>
+
+class Character {
+private:
+    std::string name;
+    int hp, maxHp, attack, defense, level, exp;
+
+public:
+    Character(std::string n = "Hero") : name(n), hp(100), maxHp(100), attack(10), defense(5), level(1), exp(0) {}
+
+    Character(std::string n, int hp_, int atk_, int def_, int maxHp_, int lvl_, int exp_)
+        : name(n), hp(hp_), attack(atk_), defense(def_), maxHp(maxHp_), level(lvl_), exp(exp_) {}
+
+    void showInfo() const {
+        std::cout << name << " - HP: " << hp << "/" << maxHp << " | ATK: " << attack
+            << " | DEF: " << defense << " | LVL: " << level << " | EXP: " << exp << "\n";
+    }
+
+    void gainExp(int amount) {
+        exp += amount;
+        if (exp >= level * 10) {
+            exp -= level * 10;
+            level++;
+            std::cout << "Level up! Now level " << level << "!\n";
+            increaseRandomStat();
+        }
+    }
+
+    void heal() {
+        restoreHP(20);
+        std::cout << name << " healed for 20 HP.\n";
+    }
+
+    void takeDamage(int dmg) {
+        int damage = dmg - defense;
+        if (damage < 0) damage = 0;
+        hp -= damage;
+        std::cout << name << " takes " << damage << " damage.\n";
+    }
+
+    void restoreHP(int amount) {
+        hp += amount;
+        if (hp > maxHp) hp = maxHp;
+    }
+
+    bool isAlive() const { return hp > 0; }
+    int getAttack() const { return attack; }
+    int getDefense() const { return defense; }
+    int getHP() const { return hp; }
+    std::string getName() const { return name; }
+
+    void increaseRandomStat() {
+        int choice = rand() % 3;
+        switch (choice) {
+        case 0: attack += 2; std::cout << "Attack increased!\n"; break;
+        case 1: defense += 2; std::cout << "Defense increased!\n"; break;
+        case 2: maxHp += 10; hp += 10; std::cout << "HP increased!\n"; break;
+        }
+    }
+
+    void status() const {
+        std::cout << "Character Status:\n";
+        showInfo();
+    }
+
+    int getMaxHP() const { return maxHp; }
+    int getLevel() const { return level; }
+    int getExp() const { return exp; }
+};
+
+template<typename T>
+class Inventory {
+private:
+    std::vector<T> items;
+
+public:
+    void addItem(const T& item) {
+        items.push_back(item);
+    }
+
+    void removeItem(const T& item) {
+        auto it = std::find(items.begin(), items.end(), item);
+        if (it != items.end()) items.erase(it);
+    }
+
+    bool contains(const T& item) const {
+        return std::find(items.begin(), items.end(), item) != items.end();
+    }
+
+    void showInventory() const {
+        std::cout << "Inventory:\n";
+        if (items.empty()) std::cout << "  (empty)\n";
+        for (const auto& item : items) std::cout << "  - " << item << "\n";
+    }
+};
+
+class Monster {
+protected:
+    std::string name;
+    int hp, attack, defense;
+
+public:
+    Monster(std::string n, int h, int a, int d) : name(n), hp(h), attack(a), defense(d) {}
+    virtual ~Monster() = default;
+
+    virtual void showInfo() const {
+        std::cout << name << " - HP: " << hp << ", ATK: " << attack << ", DEF: " << defense << "\n";
+    }
+
+    virtual void takeDamage(int dmg) {
+        int damage = dmg - defense;
+        if (damage < 0) damage = 0;
+        hp -= damage;
+        std::cout << name << " takes " << damage << " damage.\n";
+    }
+
+    bool isAlive() const { return hp > 0; }
+    int getAttack() const { return attack; }
+    int getDefense() const { return defense; }
+    std::string getName() const { return name; }
+};
+
+class Goblin : public Monster {
+public:
+    Goblin() : Monster("Goblin", 30, 5, 2) {}
+};
+
+class Skeleton : public Monster {
+public:
+    Skeleton() : Monster("Skeleton", 40, 7, 3) {}
+};
+
+class Dragon : public Monster {
+public:
+    Dragon() : Monster("Dragon", 100, 15, 5) {}
+};
+
+class Game {
+private:
+    Character player;
+    Inventory<std::string> inventory;
+    int x = 0, y = 0;
+
+    std::unique_ptr<Monster> spawnMonster() {
+        int r = rand() % 3;
+        if (r == 0) return std::make_unique<Goblin>();
+        if (r == 1) return std::make_unique<Skeleton>();
+        return std::make_unique<Dragon>();
+    }
+
+    void battle(std::unique_ptr<Monster> monster) {
+        while (player.isAlive() && monster->isAlive()) {
+            std::cout << "\n";
+            player.showInfo();
+            monster->showInfo();
+
+            std::cout << "Attack or run? ";
+            std::string action;
+            std::cin >> action;
+
+            if (action == "run") {
+                std::cout << "You escaped!\n";
+                return;
+            }
+
+            monster->takeDamage(player.getAttack());
+            if (!monster->isAlive()) break;
+            player.takeDamage(monster->getAttack());
+        }
+
+        if (player.isAlive()) {
+            std::cout << "You defeated the " << monster->getName() << "!\n";
+            player.gainExp(5 + rand() % 6);
+            player.increaseRandomStat();
+            inventory.addItem("Healing Potion");
+        }
+    }
+
+    void saveGame() {
+        std::ofstream out("save.txt");
+        if (!out) {
+            std::cerr << "Failed to save game.\n";
+            return;
+        }
+
+        out << x << " " << y << "\n";
+        out << player.getName() << "\n";
+        out << player.getHP() << " " << player.getAttack() << " " << player.getDefense() << " "
+            << player.getMaxHP() << " " << player.getLevel() << " " << player.getExp() << "\n";
+        out << inventory.contains("Healing Potion") << " " << inventory.contains("Sword") << "\n";
+        std::cout << "Game saved.\n";
+    }
+
+    void loadGame() {
+        std::ifstream in("save.txt");
+        if (!in) {
+            std::cerr << "Failed to load game.\n";
+            return;
+        }
+
+        int hp, atk, def, maxHp, lvl, exp;
+        std::string name;
+        int hasPotion, hasSword;
+        in >> x >> y;
+        in.ignore();
+        std::getline(in, name);
+        in >> hp >> atk >> def >> maxHp >> lvl >> exp;
+        in >> hasPotion >> hasSword;
+
+        player = Character(name, hp, atk, def, maxHp, lvl, exp);
+        inventory = Inventory<std::string>();
+        if (hasPotion) inventory.addItem("Healing Potion");
+        if (hasSword) inventory.addItem("Sword");
+
+        std::cout << "Game loaded.\n";
+        player.status();
+    }
+
+    void processAction() {
+        std::string cmd;
+        std::cout << "> ";
+        std::cin >> cmd;
+
+        if (cmd == "move") {
+            std::string dir;
+            std::cin >> dir;
+            if (dir == "up") y++;
+            else if (dir == "down") y--;
+            else if (dir == "left") x--;
+            else if (dir == "right") x++;
+            else {
+                std::cout << "Unknown direction.\n";
+                return;
+            }
+
+            std::cout << "You moved to (" << x << "," << y << ").\n";
+            player.gainExp(1);
+
+            if (rand() % 3 == 0) {
+                std::cout << "A monster appears!\n";
+                auto monster = spawnMonster();
+                battle(std::move(monster));
+            }
+
+        }
+        else if (cmd == "heal") {
+            if (inventory.contains("Healing Potion")) {
+                player.heal();
+                inventory.removeItem("Healing Potion");
+            }
+            else {
+                std::cout << "No potions left!\n";
+            }
+
+        }
+        else if (cmd == "inventory") {
+            inventory.showInventory();
+
+        }
+        else if (cmd == "save") {
+            saveGame();
+
+        }
+        else if (cmd == "load") {
+            loadGame();
+
+        }
+        else if (cmd == "status") {
+            player.status();
+
+        }
+        else if (cmd == "quit") {
+            exit(0);
+
+        }
+        else {
+            std::cout << "Unknown command.\n";
+        }
+    }
+
+public:
+    void start() {
+        std::cout << "Load game or new game? (load/new): ";
+        std::string choice;
+        std::cin >> choice;
+
+        if (choice == "load") {
+            loadGame();
+        }
+        else {
+            std::cout << "Enter your character's name: ";
+            std::string name;
+            std::cin.ignore();
+            std::getline(std::cin, name);
+            player = Character(name);
+            inventory.addItem("Healing Potion");
+            inventory.addItem("Sword");
+        }
+
+        std::cout << "Game started. Commands: move [dir], heal, inventory, save, load, status, quit\n";
+
+        while (true) {
+            processAction();
+            if (!player.isAlive()) {
+                std::cout << "You died. Game over.\n";
+                break;
+            }
+        }
+    }
+};
+
+int main() {
+    srand(static_cast<unsigned>(time(nullptr)));
+    Game game;
+    game.start();
+    return 0;
+}
